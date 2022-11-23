3f9f9fa27583b8ac9ad825ee55790801d49f578d
Lee Seonah <111514502+seonahaaaaaa@users.noreply.github.com>
2022-11-23 15:10:12 +0900

    Add files via upload

--- seonah/control/AdminController.java
+++ seonah/control/AdminController.java
@@ -15,172 +15,161 @@ import com.javaclass.domain.QnaVO;
 import com.javaclass.service.FaqService;
 import com.javaclass.service.ProductService;
 
-
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
 	@Autowired
-	   private ProductService productService;
-	
+	private ProductService productService;
+
 	@Autowired
-    	private FaqService faqService;
-	
-	//상품목록 페이지로 이동
+	private FaqService faqService;
+
+	// 상품목록 페이지로 이동
 	@RequestMapping("/itemList.do")
-	public String itemList(Model m) {	
+	public String itemList(Model m) {
 		List<ProductVO> list = productService.selectAllProduct();
 		m.addAttribute("result", list);
 		return "admin/itemList";
 	}
-	
-	//상품목록 조회 페이지로 이동
+
+	// 상품목록 조회 페이지로 이동
 	@RequestMapping("/selectItem.do")
-	public String selectItem() {	
+	public String selectItem() {
 		return "admin/itemList";
 	}
-	
-	
-	//상품등록 페이지로 이동
+
+	// 상품등록 페이지로 이동
 	@RequestMapping("/itemRegister.do")
-	public String itemRegister() {	
+	public String itemRegister() {
 		return "admin/itemRegister";
 	}
-	
-	//상품등록하기
+
+	// 상품등록하기
 	@RequestMapping("/insertProduct")
-	   public String insertProduct(ProductVO vo) throws IOException {
-		 System.out.println(vo.getProduct_name());
-	      System.out.println("상품등록");
-	      productService.insertProduct(vo);
-	      return "admin/itemList";
-	   }
-	
-	//상품별판매통계 페이지로 이동
+	public String insertProduct(ProductVO vo) throws IOException {
+		System.out.println(vo.getProduct_name());
+		System.out.println("상품등록");
+		productService.insertProduct(vo);
+		return "admin/itemList";
+	}
+
+	// 상품별판매통계 페이지로 이동
 	@RequestMapping("/itemCharts.do")
-	public String itemCharts() {	
+	public String itemCharts() {
 		return "admin/itemCharts";
 	}
-	
-	//정산관리 페이지로 이동
+
+	// 정산관리 페이지로 이동
 	@RequestMapping("/calCharts.do")
-	public String calCharts() {	
+	public String calCharts() {
 		return "admin/calCharts";
 	}
-	
-	//회원관리 페이지로 이동
+
+	// 회원관리 페이지로 이동
 	@RequestMapping("/member.do")
-	public String member() {	
+	public String member() {
 		return "admin/member";
 	}
-	
-	//회원상세정보 페이지로 이동
+
+	// 회원상세정보 페이지로 이동
 	@RequestMapping("/memberDetail.do")
-	public String memberDetail() {	
+	public String memberDetail() {
 		return "admin/memberDetail";
 	}
-	
-	
-	//FAQ관리 페이지로 이동
-	@RequestMapping("/faq.do")
-	public String faq() {	
-		return "admin/faq";
-	}
-	
-	//리뷰관리 페이지로 이동
+
+	// 리뷰관리 페이지로 이동
 	@RequestMapping("/review.do")
-	public String review() {	
+	public String review() {
 		return "admin/review";
 	}
-	
-	//QNA관리 페이지로 이동
+
+	// QNA관리 페이지로 이동
 	@RequestMapping("/qna.do")
-	public String qna() {	
+	public String qna() {
 		return "admin/qna";
 	}
-	
-	//공지사항관리 페이지로 이동
+
+	// 공지사항관리 페이지로 이동
 	@RequestMapping("/notice.do")
-	public String notice() {	
+	public String notice() {
 		return "admin/notice";
 	}
-	
-	//FAQ 수정 페이지로 이동
-	@RequestMapping("/modifyFaq.do")
-	public String modifyFaq() {	
-		return "admin/modifyFaq";
-	}
-	
-	//공지사항 수정 페이지로 이동
+
+	// 공지사항 수정 페이지로 이동
 	@RequestMapping("/modifyNotice.do")
-	public String modifyNotice() {	
+	public String modifyNotice() {
 		return "admin/modifyNotice";
 	}
 
-	//FAQ등록 페이지로 이동
+	// FAQ등록 페이지로 이동
 	@RequestMapping("/registFaq.do")
-	public String registFaq() {	
+	public String registFaq() {
 		return "admin/registFaq";
 	}
-	
-	//QNA 답변 등록 페이지로 이동
+
+	// QNA 답변 등록 페이지로 이동
 	@RequestMapping("/registQna.do")
-	public String registQna() {	
+	public String registQna() {
 		return "admin/registQna";
 	}
-	
-	//공지사항등록 페이지로 이동
+
+	// 공지사항등록 페이지로 이동
 	@RequestMapping("/registNotice.do")
-	public String registNotice() {	
+	public String registNotice() {
 		return "admin/registNotice";
 	}
-	
-	//주문내역관리 페이지로 이동
+
+	// 주문내역관리 페이지로 이동
 	@RequestMapping("/orderList.do")
-	public String orderList() {	
+	public String orderList() {
 		return "admin/orderList";
 	}
-	
-	
-	/****** FAQ ******/
-	
-	/*
-	 * // 페이지 이동
-	 * 
-	 * @RequestMapping("/faq.do") 
-	 * public String faq(FaqVO faqvo, Model m) {
-	 * m.addAttribute("faqList", faqService.getFaqBoardList(faqvo)); 
-	 * return
-	 * "admin/Faq"; }
-	 */
-	
 
-	// FAQ 수정하기 페이지로 이동
-//	@RequestMapping("/modifyFaq.do")
-//	public String updateFaqBoard(FaqVO faqvo, Model m) {	
-//		 FaqVO result = faqService.selectFaqBoard(faqvo);
-//		 System.out.println("result : "+result.getFaq_content());
-//		 m.addAttribute("faq", result);
-//		 return "admin/modifyFaq";
-//	}
+	/****** FAQ ******/
+
+	// 페이지 이동시 등록되어 있는 리스트 나열
+	@RequestMapping("/faq.do")
+	public String faq(FaqVO faqvo,  Model m) {
+		m.addAttribute("faqList",faqService.getFaqBoardList(faqvo));
+		return "admin/faq";
+	}
 	
-	//FAQ 새글등록 (어드민 페이지)
+	// FAQ 새글등록 (어드민 페이지)
 	@RequestMapping("/insertFaq.do")
-	public String insertFaq(FaqVO faqvo, Model m){
+	public String insertFaq(FaqVO faqvo, Model m) {
 		System.out.println("글등록");
 		faqService.insertFaqBoard(faqvo);
 		m.addAttribute("faqList", faqService.getFaqBoardList(faqvo));
 		return "admin/faq";
 	}
-	
+
+	// 글 삭제
+	@RequestMapping("/deleteFaq.do")
+	public String deleteFaq(FaqVO faqvo, Model m) {
+		System.out.println("글삭제");
+		faqService.deleteFaqBoard(faqvo);
+		m.addAttribute("faqList", faqService.getFaqBoardList(faqvo));
+		return "admin/faq";
+	}
+
+	// 게시글의 정보 수정 페이지에 가져오기
+	@RequestMapping("/modifyFaq.do")
+	public String faqUpdatelist(FaqVO faqvo, Model m) {
+		FaqVO result = faqService.selectFaqBoard(faqvo);
+		System.out.println("result : " + result.getFaq_content());
+		m.addAttribute("faq", result);
+		return "admin/modifyFaq";
+	}
+
 	// 글 수정
 	@RequestMapping("/updateFaq.do")
-	public String updateFaqBoard(FaqVO faqvo, Model m) {	
-		 System.out.println("글수정");
-		 faqService.updateFaqBoard(faqvo);
-		 m.addAttribute("faqList", faqService.getFaqBoardList(faqvo));
-		 return "admin/faq";
+	public String faqUpdate(FaqVO faqvo, Model m) {
+		System.out.println("글수정" + faqvo);
+
+		faqService.updateFaqBoard(faqvo);
+		m.addAttribute("faqList", faqService.getFaqBoardList(faqvo));
+		return "admin/faq";
 	}
-	
-	
+
 }
